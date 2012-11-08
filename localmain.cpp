d660c3ddffdf9004e87e1d4966213810eaa9ccf2
Brendan Ritter <brendan@brendanritter.(none)>
2012-11-08 12:21:33 -0500

    Made scrolling work easier if you hold down shift. Put the view in a window. STABLE

--- localmain.cpp
+++ localmain.cpp
@@ -6,6 +6,9 @@ int main()
 	noecho();
 	localmap some_map=localmap();
 	some_map.initialize();
+	//some_map.get_point(5,5).set_val('W');
+	//addch(some_map.get_point(5,5).get_val());
+	//getch();
 	some_map.print_map();
 	some_map.inspect();
 	endwin();
--- localmap.h
+++ localmap.h
@@ -1,6 +1,6 @@
 #include "entity.h"
 #include <ncurses.h>
-const int maxx=20;
+const int maxx=20; //controls the size of the local map
 const int maxy=20;
 class localmap
 {
@@ -16,14 +16,23 @@ class localmap
 		int refy;
 		int viewx;
 		int viewy;
+		int winmaxx;
+		int winmaxy;
+		int winminx;
+		int winminy;
+		WINDOW *local_win;
 };
 localmap::localmap(){}
 void localmap::initialize()
 {
-	refx=4;
-	refy=1;
+	refx=0;
+	refy=0;
 	int i=0;
 	int j=0;
+	winmaxx=80;
+	winmaxy=40;
+
+	local_win = newwin(winmaxy, winmaxx, 0, 0);
 	while (i<maxx){
 		j=0;
 		while (j<maxy){
@@ -34,72 +43,69 @@ void localmap::initialize()
 		}
 		i+=1;
 	}
+	wrefresh(local_win);
 }
-entity localmap::get_point(int x,int y){
+entity localmap::get_point(int x,int y){ 
 	return map[x][y];
 }
 void localmap::print_map()
 {
 	int i=0;
 	int j=0;
-	while (i<maxx){
+	while (i<winmaxx){ //window size x
 		j=0;
-		while (j<maxy){
-			if ((j+refy<maxy)&&(i+refx<maxx)&&(j+refy>0)&&(i+refx>0)){
-				move(j+refy,i+refx);
-				addch(map[i+refx][j+refy].get_val());
+		while (j<winmaxy){ //window size y
+			if ((i+refx<maxx)&&(i+refx>0)&&(j+refy<maxy)&&(j+refy>0)){
+				wmove(local_win,j,i);
+				waddch(local_win,map[i+refx][j+refy].get_val());
 			}else{
-				move(j+refy,i+refx);
-				addch(' ');
+				wmove(local_win,j,i);
+				waddch(local_win,' ');
 			}
 			j+=1;
 		}
 		i+=1;
 	}
-	refresh();
+	wrefresh(local_win);
 }
 void localmap::inspect(){
 		viewx=0;
 		viewy=0;
 		char c;
 		while (c!='q'){
-			c=getch();
-			move(LINES-1,1);
-			addstr("                                                  ");
-			if (c=='w'){refy-=1;}
-			if (c=='a'){refx-=1;}
-			if (c=='s'){refy+=1;}
-			if (c=='d'){refx+=1;}
+			c=wgetch(local_win);
+			if (c=='w'){viewy-=1;}
+			if (c=='a'){viewx-=1;}
+			if (c=='s'){viewy+=1;}
+			if (c=='d'){viewx+=1;}
+			if (c=='W'){viewy-=5;}
+			if (c=='A'){viewx-=5;}
+			if (c=='S'){viewy+=5;}
+			if (c=='D'){viewx+=5;}
 			
-			move(LINES-1,1);
 			if (viewx<0){
 				viewx=0;
-				addstr("bump a");
-				//printw("refx: %i",refx);
+				refx-=1;
+				if (c=='A'){refx-=3;}
 			}
 			if (viewy<0){
 				viewy=0;
-				addstr("bump b");
+				refy-=1;
+				if (c=='W'){refy-=3;}
 			}
-			if (viewx>maxx-1){
-				viewx=maxx-1;
-				addstr("bump c");
+			if (viewx>winmaxx-1){
+				viewx=winmaxx-1;
+				refx+=1;
+				if (c=='D'){refx+=3;}
 			}
-			if (viewy>maxy-1){
-				viewy=maxy-1;
-				addstr("bump d");
-			}
-			if (refy<0){
-				refy=0;
-				addstr("bump e");
-			}
-			if (refx<0){
-				refx=0;;
-				addstr("bump f");
+			if (viewy>winmaxy-1){
+				viewy=winmaxy-1;
+				refy+=1;
+				if (c=='S'){refy+=3;}
 			}
 			print_map();
-			touchwin(stdscr);
-			refresh();
-			move(viewy,viewx);
+			//touchwin(stdscr);
+			wrefresh(local_win);
+			wmove(local_win,viewy,viewx);
 		}
 }
